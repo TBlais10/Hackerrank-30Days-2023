978bc58d571b71b9aa8e31cd1cada13f495ac6b9
Taylor Blais <87341155+TBlais10@users.noreply.github.com>
2023-02-06 15:10:26 -0500

    Added link to day 0, completed day 1 of 30 Days

--- src/Challenges/Day0.java
+++ src/Challenges/Day0.java
@@ -1,5 +1,6 @@
 package Challenges;
-
+//Challenge Link
+//https://www.hackerrank.com/challenges/30-hello-world/problem?isFullScreen=true
 public class Day0 {
     public static void main(String[] args) {
         System.out.println("Welcome to 30 Days of Code!");
---
+++ src/Challenges/Day1.java
@@ -0,0 +1,18 @@
+package Challenges;
+
+//Challenge Link
+//https://www.hackerrank.com/challenges/30-data-types/problem?isFullScreen=true
+public class Day1 {
+
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+        int num = 12;
+        double dub = 4.0;
+        String str = "is the best place to learn and practice coding! ";
+
+        System.out.println((num + i) + "\n" + (d + dub) +"\n" + s + str);
+    }
+
+}
